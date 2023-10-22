c78ad8d96de0096573486e895d626c099d393ae7
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-10-22 21:57:25 +0530

    13- Roman to integer

---
+++ 13 -roman-to-integer.js
@@ -0,0 +1,34 @@
+
+function romanToInt(s) {
+    const romanNumerals = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    };
+  
+    let result = 0;
+    let prevValue = 0;
+  
+    for (let i = s.length - 1; i >= 0; i--) {
+      const currentValue = romanNumerals[s[i]];
+  
+      if (currentValue < prevValue) {
+        result -= currentValue;
+      } else {
+        result += currentValue;
+      }
+  
+      prevValue = currentValue;
+    }
+  
+    return result;
+  }
+  
+  
+  const romanNumeral = "XIV"; 
+  const integerEquivalent = romanToInt(romanNumeral);
+  console.log(`${romanNumeral} = ${integerEquivalent}`);
