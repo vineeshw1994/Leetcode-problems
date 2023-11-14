b8058008d9fd9c709917337491fe77819799ca6c
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-11-14 20:34:13 +0530

    climping stairs

---
+++ 70-climping_stairs.js
@@ -0,0 +1,20 @@
+function climbStairs(n) {
+    if (n <= 2) {
+        return n;
+    }
+
+    const dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+
+const n = 5; // Replace with the number of steps you want to climb
+const distinctWays = climbStairs(n);
+console.log(`There are ${distinctWays} distinct ways to climb ${n} stairs.`);
